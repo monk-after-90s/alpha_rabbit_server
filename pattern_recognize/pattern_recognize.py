596b3225d6afb688afc7f95b10af8151828f1887
90houlaoheshang <907333918@qq.com>
2023-06-28 21:10:06 +0800

    初步实现接口：获取全部形态组

--- pattern_recognize/pattern_recognize.py
+++ pattern_recognize/pattern_recognize.py
@@ -1,5 +1,8 @@
 from fastapi import APIRouter, Path, Request
 from utilities import MarketType
+from orm import Session, KPatternGroup
+from sqlalchemy import select
+from loguru import logger
 
 router = APIRouter()
 
@@ -9,9 +12,8 @@ async def get_all_pattern_groups(*,
                                  symbol_type: MarketType = Path(..., alias="symbolType"),
                                  request: Request):
     """获取全部形态组"""
-    return [
-        {
-            "id": 0,
-            "name": "string"
-        }
-    ]
+    async with Session() as session:
+        # 执行查询并获取全部数据
+        sql = select(KPatternGroup)
+        result = await session.execute(sql)
+    return result.scalars().all()
