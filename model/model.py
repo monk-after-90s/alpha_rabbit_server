2326a7e0453099793fdeae4996cd7240637d703a
90houlaoheshang <907333918@qq.com>
2023-06-28 18:09:00 +0800

    数据表model

---
+++ model/model.py
@@ -0,0 +1,64 @@
+from sqlalchemy import Float, Index, JSON, String, TIMESTAMP, text
+from sqlalchemy.dialects.mysql import INTEGER, TINYINT, VARCHAR
+from sqlalchemy.orm import declarative_base, mapped_column
+
+Base = declarative_base()
+
+
+class KPattern(Base):
+    __tablename__ = 'k_pattern'
+    __table_args__ = {'comment': 'K线形态'}
+
+    id = mapped_column(INTEGER, primary_key=True)
+    name = mapped_column(String(128, 'utf8mb4_bin'), nullable=False, comment='名')
+    is_delete = mapped_column(TINYINT(1), nullable=False, server_default=text("'0'"),
+                              comment='删除标识 0:未删除 1:已删除')
+    create_id = mapped_column(INTEGER, nullable=False, server_default=text("'0'"), comment='创建用户ID')
+    create_ts = mapped_column(TIMESTAMP, nullable=False, server_default=text('CURRENT_TIMESTAMP'), comment='创建时间戳')
+    update_id = mapped_column(INTEGER, nullable=False, server_default=text("'0'"), comment='更新用户ID')
+    update_ts = mapped_column(TIMESTAMP, nullable=False, server_default=text('CURRENT_TIMESTAMP'), comment='更新时间戳')
+    description = mapped_column(String(512, 'utf8mb4_bin'), comment='描述')
+    imageUrl = mapped_column(String(255, 'utf8mb4_bin'), comment='图片URL')
+
+
+class KPatternGroup(Base):
+    __tablename__ = 'k_pattern_group'
+    __table_args__ = {'comment': 'K线形态组'}
+
+    id = mapped_column(INTEGER, primary_key=True)
+    name = mapped_column(String(128, 'utf8mb4_bin'), nullable=False, comment='形态组名')
+    is_delete = mapped_column(TINYINT(1), nullable=False, server_default=text("'0'"),
+                              comment='删除标识 0:未删除 1:已删除')
+    create_id = mapped_column(INTEGER, nullable=False, server_default=text("'0'"), comment='创建用户ID')
+    create_ts = mapped_column(TIMESTAMP, nullable=False, server_default=text('CURRENT_TIMESTAMP'), comment='创建时间戳')
+    update_id = mapped_column(INTEGER, nullable=False, server_default=text("'0'"), comment='更新用户ID')
+    update_ts = mapped_column(TIMESTAMP, nullable=False, server_default=text('CURRENT_TIMESTAMP'), comment='更新时间戳')
+    description = mapped_column(String(512, 'utf8mb4_bin'), comment='描述')
+
+
+class PatternRecognizeRecord(Base):
+    __tablename__ = 'pattern_recognize_record'
+    __table_args__ = (
+        Index('unique_recognization_idx', 'patternId', 'symbol_type', 'symbol', 'kInterval', 'patternStart',
+              'patternEnd', unique=True),
+        {'comment': '形态识别记录'}
+    )
+
+    id = mapped_column(INTEGER, primary_key=True)
+    exchange = mapped_column(String(32, 'utf8mb4_bin'), nullable=False, comment='交易所：如 币安,BINANCE')
+    symbol_type = mapped_column(String(16, 'utf8mb4_bin'), nullable=False, comment='市场类型:spot、futures')
+    symbol = mapped_column(VARCHAR(128), nullable=False, comment='市场符号，如BTC/USDT')
+    kInterval = mapped_column(VARCHAR(10), nullable=False, comment='K线Interval，如1d，4h')
+    patternId = mapped_column(INTEGER, nullable=False, comment='匹配的形态id ')
+    patternStart = mapped_column(TIMESTAMP, nullable=False, server_default=text('CURRENT_TIMESTAMP'),
+                                 comment='形态匹配的起始K线开盘时间戳')
+    patternEnd = mapped_column(TIMESTAMP, nullable=False, server_default=text('CURRENT_TIMESTAMP'),
+                               comment='形态匹配的终止K线开盘时间戳')
+    matchScore = mapped_column(Float, nullable=False, comment='匹配度 ')
+    is_delete = mapped_column(TINYINT(1), nullable=False, server_default=text("'0'"),
+                              comment='删除标识 0:未删除 1:已删除')
+    create_id = mapped_column(INTEGER, nullable=False, server_default=text("'0'"), comment='创建用户ID')
+    create_ts = mapped_column(TIMESTAMP, nullable=False, server_default=text('CURRENT_TIMESTAMP'), comment='创建时间戳')
+    update_id = mapped_column(INTEGER, nullable=False, server_default=text("'0'"), comment='更新用户ID')
+    update_ts = mapped_column(TIMESTAMP, nullable=False, server_default=text('CURRENT_TIMESTAMP'), comment='更新时间戳')
+    extra = mapped_column(JSON, comment='匹配形态结果的其他返回值')
