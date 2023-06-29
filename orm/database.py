54731bd0409aa1510bf007af35b9acc9ff4f6acc
90houlaoheshang <907333918@qq.com>
2023-06-29 18:34:39 +0800

    数据库连接的数据集和时区

--- orm/database.py
+++ orm/database.py
@@ -3,6 +3,8 @@ from setting import DB_HOST, DB_PORT, DB_USER, DB_PASSWD, DB
 from sqlalchemy.engine import URL
 
 # 创建异步SQLAlchemy引擎
-engine: AsyncEngine = create_async_engine(URL.create("mysql+aiomysql", DB_USER, DB_PASSWD, DB_HOST, DB_PORT, DB),
-                                          echo=True)
+engine: AsyncEngine = create_async_engine(
+    URL.create("mysql+aiomysql", DB_USER, DB_PASSWD, DB_HOST, DB_PORT, DB,
+               query={"charset": "utf8mb4", "init_command": "SET time_zone = '+8:00';"}),
+    echo=True)
 async_session = async_sessionmaker(engine, expire_on_commit=False)
