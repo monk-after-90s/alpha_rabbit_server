ab12a14c6d637b90814256ebadab401f603fb44a
90houlaoheshang <907333918@qq.com>
2023-06-29 19:17:51 +0800

    形态识别API

--- pattern_recognize/routes.py
+++ pattern_recognize/routes.py
@@ -1,9 +1,13 @@
+import datetime
+
 from fastapi import APIRouter, Path, Request
 from utilities import MarketType
-from orm import KPatternGroup
+from orm import KPatternGroup, PatternRecognizeRecord
 from sqlalchemy import select
 from orm import async_session
 from sqlalchemy.orm import selectinload
+from dateutil.parser import parse
+from utilities import convert_to_sh
 
 router = APIRouter()
 
@@ -32,4 +36,67 @@ async def get_grp_patterns(request: Request):
             "id": pattern_grp.id,
             "name": pattern_grp.name,
             "patterns": pattern_grp.k_patterns
+        }
+
+
+@router.post("/{symbolType}/recognizePattern")
+async def recognize_pattern(*,
+                            symbol_type: MarketType = Path(..., alias="symbolType"),
+                            request: Request):
+    """形态识别"""
+    async with async_session() as session:
+        # 市场类型
+        stmt = select(PatternRecognizeRecord).where(
+            PatternRecognizeRecord.symbol_type == str(symbol_type.value)).order_by(PatternRecognizeRecord.id)
+        # 分页
+        page: int = 1
+        per_page: int = 10
+        # 处理post参数
+        recognize_param = await request.json()
+        # 参数搜集
+        ## 普通参数
+        if 'page' in recognize_param:
+            page = recognize_param['page']
+        if 'per_page' in recognize_param:
+            per_page = recognize_param['per_page']
+        if 'exchange' in recognize_param:
+            stmt = stmt.where(PatternRecognizeRecord.exchange == str(recognize_param['exchange']))
+        if 'symbol' in recognize_param:
+            stmt = stmt.where(PatternRecognizeRecord.symbol == str(recognize_param['symbol']))
+        if 'kInterval' in recognize_param:
+            stmt = stmt.where(PatternRecognizeRecord.kInterval == str(recognize_param['kInterval']))
+        if 'start' in recognize_param:
+            stmt = stmt.where(PatternRecognizeRecord.patternEnd >= convert_to_sh(parse(recognize_param['start'])))
+        if 'end' in recognize_param:
+            stmt = stmt.where(PatternRecognizeRecord.patternEnd <= convert_to_sh(parse(recognize_param['end'])))
+        ## patternIds
+        if 'patternIds' in recognize_param:
+            pattern_ids = recognize_param['patternIds']
+
+            stmt = stmt.where(PatternRecognizeRecord.id.in_(pattern_ids))
+
+        # 计算需要跳过的项的数量
+        skip = (page - 1) * per_page
+        stmt = stmt.offset(skip).limit(per_page)
+        # 执行语句
+        result = await session.execute(stmt)
+        pattern_recognize_records = result.scalars().all()
+
+        # 统计
+        start = datetime.datetime(3000, 1, 1)
+        end = datetime.datetime(1970, 1, 1)
+        for pattern_recognize_record in pattern_recognize_records:
+            if pattern_recognize_record.patternEnd < start:
+                start = pattern_recognize_record.patternEnd
+            if pattern_recognize_record.patternEnd > end:
+                end = pattern_recognize_record.patternEnd
+        return {
+            'start': start,
+            'end': end,
+            'len': len(pattern_recognize_records),
+            'data': pattern_recognize_records,
+            'pagination': {
+                "current_page": page,
+                "per_page": per_page,
+            }
         }
