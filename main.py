2bdb0be14ba458636dcdfd040de180b0f535aa96
90houlaoheshang <907333918@qq.com>
2023-06-29 13:58:54 +0800

    优雅退出SQLAlchemy 数据库连接

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
 from smart_predict import router as smart_predict_router
 from pattern_recognize import router as pattern_recognize_router
+from orm import engine
 
 app = FastAPI()
+
+
+@app.on_event("shutdown")
+async def shutdown_event():
+    # 关闭数据库连接
+    await engine.dispose()
+
 
 app.include_router(pattern_recognize_router)
 app.include_router(smart_predict_router)
