fbd5f700e9744a8a31e3b5222f7a7dc78f54beff
90houlaoheshang <907333918@qq.com>
2023-06-30 16:36:36 +0800

    应用.env配置文件

--- orm/database.py
+++ orm/database.py
@@ -1,15 +1,23 @@
+import os
+
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncEngine, async_sessionmaker
-from setting import DB_HOST, DB_PORT, DB_USER, DB_PASSWD, DB
 from sqlalchemy.engine import URL
 from typing import Any
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.engine import Result
 from sqlalchemy.sql.base import Executable
 from sqlalchemy.sql import Select
+from dotenv import load_dotenv
 
+load_dotenv()
 # 创建异步SQLAlchemy引擎
 engine: AsyncEngine = create_async_engine(
-    URL.create("mysql+aiomysql", DB_USER, DB_PASSWD, DB_HOST, DB_PORT, DB,
+    URL.create("mysql+aiomysql",
+               os.getenv("DB_USER"),
+               os.getenv("DB_PASSWD"),
+               os.getenv("DB_HOST"),
+               os.getenv("DB_PORT"),
+               os.getenv("DB"),
                query={"charset": "utf8mb4", "init_command": "SET time_zone = '+08:00';"}),
     echo=True)
 
