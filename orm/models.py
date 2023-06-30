253155ec6c28582e736067444d009d81e60f7c65
90houlaoheshang <907333918@qq.com>
2023-06-30 13:48:11 +0800

    记录ToDo任务

--- orm/models.py
+++ orm/models.py
@@ -7,6 +7,7 @@ from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.ext.asyncio import AsyncAttrs
 
 
+# todo 全部实现is_delete逻辑删除
 class CommonColumn(AsyncAttrs, DeclarativeBase):
     __abstract__ = True
 
