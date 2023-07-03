07345947fa109f76c89aedc85046ef9c9a30fb26
90houlaoheshang <907333918@qq.com>
2023-07-03 16:49:08 +0800

    组件：vnpy INTERVALS到秒数的转换

--- utilities/__init__.py
+++ utilities/__init__.py
@@ -3,3 +3,6 @@ from .convert_symbol import init_symbol_mapping, symbol_vnpy2united, symbol_unit
 from .convert_datetime import convert_to_sh, convert_to_utc
 from .timezone_timestamp import TimestampWithTimezone
 from .convert_interval import bn_to_vnpy_interval
+
+# vnpy INTERVALS到秒数的转换
+INTERVAL_SECS_MAP = {"d": 86_400, "4h": 14_400, "1h": 3_600, "30m": 1_800}
