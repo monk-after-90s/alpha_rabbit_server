170cf1907326ea3a048e97de0188fd665879a228
90houlaoheshang <907333918@qq.com>
2023-07-03 16:48:20 +0800

    通用组件：币安标准的interval到vnpy标准

--- utilities/__init__.py
+++ utilities/__init__.py
@@ -2,3 +2,4 @@ from .market_type_enum import MarketType
 from .convert_symbol import init_symbol_mapping, symbol_vnpy2united, symbol_united2vnpy
 from .convert_datetime import convert_to_sh, convert_to_utc
 from .timezone_timestamp import TimestampWithTimezone
+from .convert_interval import bn_to_vnpy_interval
