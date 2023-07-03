68e9cf3262c3c05065eb8af73f8e8ec5124c1bc2
90houlaoheshang <907333918@qq.com>
2023-07-03 14:44:54 +0800

    如果有字段is_delete作为逻辑删除的标识，忽视被逻辑删除的条目的异步会话类

--- orm/database.py
+++ orm/database.py
@@ -1,5 +1,4 @@
 import os
-
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncEngine, async_sessionmaker
 from sqlalchemy.engine import URL
 from typing import Any
@@ -12,8 +11,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-class LogicDelAsyncSession(AsyncSession):
-    """忽视被逻辑删除的条目的异步会话"""
+class SoftLogicDelAsyncSession(AsyncSession):
+    """如果有字段is_delete作为逻辑删除的标识，忽视被逻辑删除的条目的异步会话"""
 
     async def execute(
             self,
@@ -21,7 +20,7 @@ class LogicDelAsyncSession(AsyncSession):
             *args,
             **kw: Any,
     ) -> Result[Any]:
-        if isinstance(statement, Select):
+        if isinstance(statement, Select) and hasattr(statement.c, 'is_delete'):
             statement = statement.filter_by(is_delete=0)
         return await super().execute(statement, *args, **kw)
 
@@ -37,7 +36,7 @@ engine0: AsyncEngine = create_async_engine(
                query={"charset": "utf8mb4", "init_command": "SET time_zone = '+08:00';"}),
     echo=True)
 # 0号数据库的会话工厂
-async_session_of_db0 = async_sessionmaker(engine0, expire_on_commit=False, class_=LogicDelAsyncSession)
+async_session_of_db0 = async_sessionmaker(engine0, expire_on_commit=False, class_=SoftLogicDelAsyncSession)
 # 1号数据库 创建异步SQLAlchemy引擎
 engine1: AsyncEngine = create_async_engine(
     URL.create("mysql+aiomysql",
@@ -49,4 +48,4 @@ engine1: AsyncEngine = create_async_engine(
                query={"charset": "utf8mb4", "init_command": "SET time_zone = '+08:00';"}),
     echo=True)
 # 1号数据库的会话工厂
-async_session_of_db1 = async_sessionmaker(engine1, expire_on_commit=False)
+async_session_of_db1 = async_sessionmaker(engine1, expire_on_commit=False, class_=SoftLogicDelAsyncSession)
