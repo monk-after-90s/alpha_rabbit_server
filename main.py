e0d0e6a1f66577fb172d45e817759d5557294856
90houlaoheshang <907333918@qq.com>
2023-07-06 18:26:11 +0800

    优雅关闭所有SQLAlchemy引擎

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import uvicorn
 from fastapi import FastAPI
 from routers.smart_predict import router as smart_predict_router, executor
 from routers.pattern_recognize import router as pattern_recognize_router
-from orm import engine0
+from orm import engine0, engine1
+import asyncio
 
 app = FastAPI()
 
@@ -10,7 +11,7 @@ app = FastAPI()
 @app.on_event("shutdown")
 async def shutdown_event():
     # 关闭数据库连接
-    await engine0.dispose()
+    await asyncio.gather(engine0.dispose(), engine1.dispose())
     executor.shutdown()
 
 
