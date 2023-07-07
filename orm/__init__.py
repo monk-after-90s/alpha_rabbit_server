5cee96a545548a8b02f133ac676f6e04001f68b5
90houlaoheshang <907333918@qq.com>
2023-07-07 14:03:21 +0800

    文档

--- orm/__init__.py
+++ orm/__init__.py
@@ -1,2 +1,3 @@
+# SQLAlchemy ORM
 from .models import KPattern, KPatternGroup, PatternRecognizeRecord, Dbbardata
 from .database import async_session_of_db0, async_session_of_db1, engine0, engine1
--- prediction_engine/__init__.py
+++ prediction_engine/__init__.py
@@ -1,2 +1,3 @@
+# 智能预测引擎，计算密集型
 from .Basic_Prediction import Basic_Prediction
 from .Enhanced_Prediction import Enhanced_Prediction
